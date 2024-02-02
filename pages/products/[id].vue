a2a36af41ec4f1fd29ae206514c844875762cb67
xdong <none@none.no>
2024-02-02 19:32:53 +0900

    initial commit

---
+++ pages/products/[id].vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>
+      {{ product.title }}
+    </p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
+
+  </div>
+</template>
+
+<script setup>
+  const {id} = useRoute().params
+  const uri = 'https://fakestoreapi.com/products/' + id
+
+  const {data: product} = await useFetch(uri)
+
+  definePageMeta({
+    layout: 'products'
+  })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <div v-for="p in products">
+      <ProductCard :product="p" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: 'products'
+  })
+
+  const {data: products} = await useFetch('https://fakestoreapi.com/products')
+</script>
+
+<style lang="scss" scoped>
+
+</style>
